542a8f9e7508b1a635864984e85d3de27f53ff12
Apdo3939 <apdo3939@hotmail.com>
2021-01-16 15:30:49 -0300

    Project created

---
+++ backend/src/test/java/com/apdo3939/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apdo3939.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
