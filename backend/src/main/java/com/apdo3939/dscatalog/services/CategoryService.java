e63b261fca3237a705e20662ac1a519bd0a39490
Apdo3939 <apdo3939@hotmail.com>
2021-01-18 17:33:11 -0300

    Delete category

--- backend/src/main/java/com/apdo3939/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/apdo3939/dscatalog/services/CategoryService.java
@@ -7,31 +7,33 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.apdo3939.dscatalog.dto.CategoryDTO;
 import com.apdo3939.dscatalog.entities.Category;
 import com.apdo3939.dscatalog.repositories.CategoryRepository;
+import com.apdo3939.dscatalog.services.exceptions.DataBaseException;
 import com.apdo3939.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		
+	public List<CategoryDTO> findAll() {
+
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
-		/*List<CategoryDTO> listDto = new ArrayList<>();	
-		for(Category cat : list) {
-			listDto.add(new CategoryDTO(cat));
-		}
-		return listDto;*/
+
+		/*
+		 * List<CategoryDTO> listDto = new ArrayList<>(); for(Category cat : list) {
+		 * listDto.add(new CategoryDTO(cat)); } return listDto;
+		 */
 	}
 
 	@Transactional(readOnly = true)
@@ -56,9 +58,24 @@ public class CategoryService {
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
+
 		} catch (EntityNotFoundException e) {
 			e.getMessage();
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			e.getMessage();
+			throw new ResourceNotFoundException("Id not found " + id);
+			
+		} catch (DataIntegrityViolationException e) {
+			e.getMessage();
+			throw new DataBaseException("Integrety Violation " + id);
 		}
 	}
 
