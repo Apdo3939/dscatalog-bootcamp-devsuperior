8633b16d7d1d02f67e0b6b2bb8b5a62e2936f79b
Apdo3939 <apdo3939@hotmail.com>
2021-01-25 17:37:47 -0300

    Implements Catalog products details by backend

--- front-web/src/pages/Catalogo/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalogo/components/ProductDetails/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import {ReactComponent as ArrowIcon} from '../../../../core/assets/images/seta.svg';
-import {ReactComponent as ProductImage} from '../../../../core/assets/images/product.svg';
+import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/seta.svg';
 import './styles.scss';
 import ProductPrice from '../../../../core/components/ProductPrice';
+import makeRequest from '../../../../core/utils/request';
+import { Product } from '../../../../core/Types/Product';
 
 type ParamsType = {
     productId: string;
@@ -11,35 +12,42 @@ type ParamsType = {
 
 const ProductDetails = () => {
 
-    const {productId} = useParams<ParamsType>();
+    const { productId } = useParams<ParamsType>();
     console.log(productId);
-    return(
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+        makeRequest({url:`/products/${productId}`})
+        .then(response => setProduct(response.data));
+    },[productId]);
+
+    return (
         <div className="products-details-container">
             <div className="card-base border-radius-20 products-details">
-                <Link to= "/products" className="products-details-goback">
-                <ArrowIcon className="icon-goback" />
-                <h1 className="text-goback">VOLTAR</h1>
+                <Link to="/products" className="products-details-goback">
+                    <ArrowIcon className="icon-goback" />
+                    <h1 className="text-goback">VOLTAR</h1>
                 </Link>
                 <div className="row">
                     <div className="col-6 pr-5">
                         <div className="products-details-card text-center">
-                            <ProductImage className="products-details-image" />
-                        </div>  
-                        <h1 className="products-details-name">Computador Desktop - Intel Core i7</h1>
-                        <ProductPrice price="2.779,00" />
+                            <img src={product?.imgUrl} alt={product?.name} className="products-details-image"/>
+                        </div>
+                        <h1 className="products-details-name">{product?.name}</h1>
+                        {product?.price && <ProductPrice price={product?.price} />}
                     </div>
                     <div className="col-6 products-details-card">
                         <h1 className="products-description-title">Descrição do Produto</h1>
                         <p className="products-description-text">
-                        Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela. A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja. Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.
+                            {product?.description}
                         </p>
                     </div>
                 </div>
-            </div>           
+            </div>
         </div>
     );
 }
-   
+
 
 
 export default ProductDetails;
--- front-web/src/pages/Catalogo/index.tsx
+++ front-web/src/pages/Catalogo/index.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
+import makeRequest from '../../core/utils/request';
+import { ProductResponse } from '../../core/Types/Product';
 
-const Catalogo = () => (
-    <div className="catalog-container">
-        <h1 className="catalog-title">Catalogo de produtos</h1>
-        <div className="catalog-products">
-            <Link to="/products/1"><ProductCard /></Link>
-            <Link to="/products/2"><ProductCard /></Link>
-            <Link to="/products/3"><ProductCard /></Link>
-            <Link to="/products/4"><ProductCard /></Link>
-            <Link to="/products/5"><ProductCard /></Link>
-            <Link to="/products/6"><ProductCard /></Link>
-            <Link to="/products/7"><ProductCard /></Link>
-            <Link to="/products/8"><ProductCard /></Link>
-            <Link to="/products/9"><ProductCard /></Link>
-            <Link to="/products/10"><ProductCard /></Link>
+const Catalogo = () => {
+
+    const [productResponse, setProductResponse] = useState<ProductResponse>();
+    console.log(productResponse);
+
+    useEffect(() => {
+        const params = {
+            page: 0,
+            linesPerPage: 8
+        }
+        makeRequest({ url: '/products', params })
+            .then(response => setProductResponse(response.data));
+    }, []);
+
+    return (
+        <div className="catalog-container">
+            <h1 className="catalog-title">Catalogo de produtos</h1>
+            <div className="catalog-products">
+                {productResponse?.content.map(product => (
+                    <Link to={`/products/${product.id}`} key= {product.id}>
+                        <ProductCard product={product} />
+                    </Link>
+                ))}
+
+            </div>
         </div>
-    </div>  
-);
+    );
+}
+
+
 
 export default Catalogo;
