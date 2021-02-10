c383387fe091d9f20a19d9418a1fd041fb3b5d44
Apdo3939 <apdo3939@hotmail.com>
2021-02-10 19:20:42 -0300

    Add OAuth2 implemention server

--- backend/src/main/java/com/apdo3939/dscatalog/services/UserService.java
+++ backend/src/main/java/com/apdo3939/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +30,9 @@ import com.apdo3939.dscatalog.services.exceptions.DataBaseException;
 import com.apdo3939.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -97,6 +104,19 @@ public class UserService {
 			e.getMessage();
 			throw new DataBaseException("Integrety Violation " + id);
 		}
+	}
+
+	//UserDetailsService
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if(username == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("Email found: " + username);
+		return user;
 	}
 
 }
