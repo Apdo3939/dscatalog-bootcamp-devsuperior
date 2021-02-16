627e74ed966084c0e05e336ad64cca9dd455fcc8
Apdo3939 <apdo3939@hotmail.com>
2021-02-16 18:33:19 -0300

    First test img uploads

--- backend/src/main/java/com/apdo3939/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/apdo3939/dscatalog/DscatalogApplication.java
@@ -1,13 +1,24 @@
 package com.apdo3939.dscatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DscatalogApplication {
+import com.apdo3939.dscatalog.services.S3Service;
 
+@SpringBootApplication
+public class DscatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		service.uploadFile("C:\\Java\\test.jpg");	
+	}
 }
